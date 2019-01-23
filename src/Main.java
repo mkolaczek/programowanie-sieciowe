0c8ca6ee65e430708bd8d3d2189f4367c3f050c8
Marcin <marcin.kolaczek@yahoo.pl>
2019-01-23 19:53:27 +0100

    wersja 3 działajaca na 3 :)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.net.*;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class ListenThread implements Runnable {
 
@@ -10,9 +12,7 @@ class ListenThread implements Runnable {
     private String nick;
 
 
-    public ListenThread(InetAddress group, MulticastSocket socket, String nick) {
-        this.group = group;
-        this.socket = socket;
+    public ListenThread(String nick) {
         this.nick = nick;
         t = new Thread(this);
         t.start();
@@ -21,8 +21,21 @@ class ListenThread implements Runnable {
     @Override
     public void run() {
 
-        System.out.println("watek odbierajacy");
+//        System.out.println("watek odbierajacy");
 
+        InetAddress group = null;
+        try {
+            group = InetAddress.getByName("228.5.6.7");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        MulticastSocket socket = null;
+        try {
+            socket = new MulticastSocket(6789);
+            socket.joinGroup(group);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         byte[] buf = new byte[1024];
 
@@ -33,13 +46,22 @@ class ListenThread implements Runnable {
             try {
                 socket.receive(recv);
                 String received = new String(recv.getData(), 0, recv.getLength());
-                System.out.println("odebrano: " + received);
+
+                Pattern compiledPattern = Pattern.compile("NICK");
+                Matcher matcher = compiledPattern.matcher(received);
+
                 if (received.equals(nick)) {
                     String nickBusy = nick + " BUSY";
                     DatagramPacket dp = new DatagramPacket(nickBusy.getBytes(), nickBusy.length(),
                             group, 6789);
                     socket.send(dp);
-                    System.out.println("wyslano nick busy");
+                } else if (received.equals(nick + " BUSY")) {
+                    // nie rob nic
+                } else if (matcher.find()){
+                    // nie rob nic
+                }
+                else {
+                    System.out.println(received);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -53,11 +75,11 @@ class SendThread implements Runnable {
 
     private MulticastSocket socket;
     private InetAddress group;
+    private String nick;
     Thread t;
 
-    public SendThread(InetAddress group, MulticastSocket socket) {
-        this.group = group;
-        this.socket = socket;
+    public SendThread(String nick) {
+        this.nick = nick;
         t = new Thread(this);
         t.start();
     }
@@ -65,20 +87,35 @@ class SendThread implements Runnable {
     @Override
     public void run() {
 
-        System.out.println("watek wysylajacy");
+//        System.out.println("watek wysylajacy");
+
+        InetAddress group = null;
+        try {
+            group = InetAddress.getByName("228.5.6.7");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        MulticastSocket socket = null;
+        try {
+            socket = new MulticastSocket(6789);
+            socket.joinGroup(group);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         Scanner in = new Scanner(System.in);
 
         while (true) {
 
+
+            nick = nick.substring(4, nick.length());
             String msg = in.nextLine();
+            msg = "MSG " + nick + ": " + msg;
             DatagramPacket dp = new DatagramPacket(msg.getBytes(), msg.length(),
                     group, 6789);
 
             try {
                 socket.send(dp);
-                System.out.println("wyslano");
-
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -90,7 +127,7 @@ class SendThread implements Runnable {
 
 public class Main {
 
-    public static String getNick(InetAddress group, MulticastSocket socket) {
+    private static String getNick(InetAddress group, MulticastSocket socket) {
 
         Scanner in = new Scanner(System.in);
         System.out.println("podaj nick:");
@@ -101,13 +138,50 @@ public class Main {
 
         try {
             socket.send(dp);
-            System.out.println("wyslano nick");
+//            System.out.println("wyslano nick");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         return nick;
 
+    }
+
+    private static String loop(InetAddress group, MulticastSocket socket) throws IOException {
+
+        String nick = getNick(group, socket);
+
+        socket.setSoTimeout(10000);
+
+        int condition = 0;
+
+        while (true) {
+
+
+            byte[] buf = new byte[1024];
+            DatagramPacket dp = new DatagramPacket(buf, buf.length);
+            String received = "";
+
+            try {
+                socket.receive(dp);
+                received = new String(dp.getData(), 0, dp.getLength());
+//                System.out.println("odebrano: " + received);
+            } catch (SocketTimeoutException e) {
+                condition = 1;
+            }
+
+            if ((received.equals(nick + " BUSY"))) {
+                System.out.println(nick + " BUSY");
+                nick = loop(group, socket);
+                break;
+            }
+
+            if (condition == 1)
+                break;
+        }
+
+
+        return nick;
     }
 
     public static void main(String[] args) throws IOException {
@@ -122,30 +196,13 @@ public class Main {
         socket.joinGroup(group);
 
         Scanner in = new Scanner(System.in);
-        String nick = getNick(group, socket);
+        String nick = loop(group, socket);
+
+        System.out.println("zostałeś zarejestrowany!");
 
 
-        while (true) {
-
-
-            byte[] buf = new byte[1024];
-            DatagramPacket dp = new DatagramPacket(buf, buf.length);
-            String received = "";
-
-                socket.receive(dp);
-                received = new String(dp.getData(), 0, dp.getLength());
-                System.out.println("odebrano: " + received);
-
-            if ((received.equals(nick + " BUSY"))) {
-                System.out.println(nick + " BUSY");
-//                nick = getNick(group, socket);
-                break;
-            }
-        }
-
-
-        SendThread w1 = new SendThread(group, socket);
-        ListenThread w2 = new ListenThread(group, socket, nick);
+        SendThread w1 = new SendThread(nick);
+        ListenThread w2 = new ListenThread(nick);
 
         try {
             w1.t.join();
